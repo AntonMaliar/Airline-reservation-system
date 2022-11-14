f4ccafb909fc910b3aca31a6955af575287947b6
AntonMaliar <antonmaliar0@gmail.com>
2022-11-14 17:33:44 +0200

    rewrite test for welcomePageTest

--- src/test/java/anton/maliar/AirlineReservationSystem/authentication/AuthenticationTest.java
+++ src/test/java/anton/maliar/AirlineReservationSystem/authentication/AuthenticationTest.java
@@ -1,39 +1,33 @@
 package anton.maliar.AirlineReservationSystem.authentication;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.ValueWrapper;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AuthenticationTest {
-    private URL url;
-    private HttpURLConnection con;
+    private HttpClient client;
+    private HttpResponse<String> response;
 
     @Test
     public void welcomePageTest() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080"))
+                .build();
 
         try {
-            url = new URL("http://localhost:8080");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            con = (HttpURLConnection) url.openConnection();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            assertEquals(200 , response.statusCode());
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        try {
-            con.setRequestMethod("GET");
-            assertEquals(200, con.getResponseCode());
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
