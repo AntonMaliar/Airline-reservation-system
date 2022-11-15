4e89efe5a7f1b2d2b990b61528b3d153f88ee992
AntonMaliar <antonmaliar0@gmail.com>
2022-11-15 19:15:57 +0200

    create class AuthenticationManeger and implement methods: addSession(), isSessionValid()

---
+++ src/test/java/anton/maliar/AirlineReservationSystem/authentication/AuthenticationManagerTest.java
@@ -0,0 +1,46 @@
+package anton.maliar.AirlineReservationSystem.authentication;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import javax.servlet.http.HttpSession;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+public class AuthenticationManagerTest {
+    private AuthenticationManager manager;
+
+    @BeforeEach
+    public void initAuthenticationManager(){
+        manager = new AuthenticationManager();
+    }
+    @Test
+    public void addSessionTest(){
+        HttpSession session = Mockito.mock(HttpSession.class);
+        assertTrue(manager.addSession(session));
+    }
+    @Test
+    public void isSessionValidTest(){
+        HttpSession session = Mockito.mock(HttpSession.class);
+        when(session.getId()).thenReturn("1");
+        manager.addSession(session);
+
+        try {
+            assertTrue(manager.isSessionValid(session));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void isSessionValidTestException(){
+        HttpSession session = Mockito.mock(HttpSession.class);
+        HttpSession session1 = Mockito.mock(HttpSession.class);
+        when(session.getId()).thenReturn("1");
+        when(session1.getId()).thenReturn("2");
+
+        manager.addSession(session);
+
+        Exception exception = assertThrows(Exception.class, () -> manager.isSessionValid(session1));
+        assertEquals("Session not found", exception.getMessage());
+    }
+}
